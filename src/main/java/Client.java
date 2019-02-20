1665485e3b2c8c3a2bfc77d05f9bbc1ba3f7cec7
lwh <2391519868@qq.com>
2019-02-20 09:56:45 +0800

    TCP的客户端实现

---
+++ src/main/java/Client.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+/**
+ * @author lwh
+ * @date 2019-02-20
+ * @desp
+ */
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket();
+
+        //设置读取时间超时为3000ms,我理解是服务器在3000ms内没有接受到客户端数据就强制断掉客户端连接
+        socket.setSoTimeout(3000);
+
+        //连接的服务器地址和端口号,这里连接的是本机地址,连接超时时间为3000ms
+        socket.connect(new InetSocketAddress(Inet4Address.getLocalHost(), 2000), 3000);
+
+        System.out.println("已发起服务器连接,并进入后续流程");
+        System.out.println("客户端信息: " + socket.getLocalAddress() + ", " + socket.getLocalPort());
+        System.out.println("服务端信息: " + socket.getInetAddress() + ", " + socket.getPort());
+
+        try {
+            //发送接收数据,是阻塞的操作
+            sendData(socket);
+        } catch (IOException e) {
+            System.out.println("异常关闭");
+        }
+
+        //释放资源
+        socket.close();
+        System.out.println("客户端已退出");
+    }
+
+    private static void sendData(Socket client) throws IOException{
+        InputStream in = System.in;
+        BufferedReader input = new BufferedReader(new InputStreamReader(in));
+
+        //得到Socket输出流,并转换为打印流
+        OutputStream outputStream = client.getOutputStream();
+        PrintStream socketPrintStream = new PrintStream(outputStream);
+
+        //得到Socket输入流,并转换为BufferedReader
+        InputStream inputStream = client.getInputStream();
+        BufferedReader socketBufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        boolean flag = true;
+        do{
+            //键盘读取一行
+            String str = input.readLine();
+            //发送到服务器
+            socketPrintStream.println(str);
+
+            //从服务器接收一行数据
+            String echo = socketBufferedReader.readLine();
+            if("echo".equalsIgnoreCase(echo)){
+                flag = false;
+            }else {
+                System.out.println(echo);
+            }
+        }while (flag);
+
+        //资源释放
+        socketPrintStream.close();
+        socketBufferedReader.close();
+    }
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,7 @@
+/**
+ * @author lwh
+ * @date 2019-02-20
+ * @desp
+ */
+public class Server {
+}
