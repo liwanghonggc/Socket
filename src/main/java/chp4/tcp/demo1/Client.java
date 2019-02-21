14c7bdd96c5d37c61bcd99398913400135eed01b
lwh <2391519868@qq.com>
2019-02-21 14:42:02 +0800

    TCP案例一创建Socket的初始化操作

--- src/main/java/chp4/tcp/demo1/Client.java
+++ src/main/java/chp4/tcp/demo1/Client.java
@@ -1,10 +1,10 @@
 package chp4.tcp.demo1;
 
+import sun.util.locale.provider.LocaleProviderAdapter;
+
 import java.io.*;
-import java.net.Inet4Address;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.Socket;
+import java.net.*;
+import java.time.LocalDateTime;
 
 /**
  * @author lwh
@@ -45,23 +45,69 @@ public class Client {
      * 创建Socket的一些操作
      * @return
      */
-    private static Socket createSocket(){
+    private static Socket createSocket() throws IOException{
 
+        /*
         //无代理模式,等效于空构造函数
         Socket socket = new Socket(Proxy.NO_PROXY);
 
+        //新建一份具有HTTP代理的套接字,传输数据将通过www.baidu.com:8800端口转发
+        //这里仅是以百度为例,实际中通常需要自己建立好这样一个代理服务器,在代理服务器上做好代理配置,这也是翻墙的原理
+        //如通过香港的一个可以访问外网的代理服务器为中介
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(Inet4Address.getByName("www.baidu.com"), 8800));
+        socket = new Socket(proxy);
 
+        //新建一个套接字,并且直接连接到本地20000的服务器上
+        socket = new Socket("localhost", PORT);
 
+        //新建一个套接字,并且直接连接到本地20000的服务器上
+        socket = new Socket(Inet4Address.getLocalHost(), PORT);
 
-        return null;
+        //新建一个套接字,并且直接连接到本地20000的服务器上,且绑定到本地20001的端口上
+        //这种方式是既绑定了本地地址端口,又建立了远程连接,相当于也调用了connect
+        //不推荐这种方式,因为可能需要在建立连接之前做一些设置
+        socket = new Socket("localhost", PORT, Inet4Address.getLocalHost(), LOCAL_PORT);
+        socket = new Socket(Inet4Address.getLocalHost(), PORT, Inet4Address.getLocalHost(), LOCAL_PORT);
+        */
+
+        Socket socket = new Socket();
+        //绑定到本地20001端口
+        socket.bind(new InetSocketAddress(Inet4Address.getLocalHost(), LOCAL_PORT));
+
+        return socket;
     }
 
     /**
      * 初始化Socket的一些操作
      * @param socket
      */
-    private static void initSocket(Socket socket){
+    private static void initSocket(Socket socket) throws SocketException {
+        //设置读取超时时间为2秒
+        socket.setSoTimeout(2000);
 
+        //是否复用未完全关闭的Socket地址,对于指定bind操作后的套接字有效
+        socket.setReuseAddress(true);
+
+        //是否开启Nagle算法
+        socket.setTcpNoDelay(false);
+
+        //是否需要在长时间无数据响应时发送确认数据(心跳包),时间大约2小时
+        socket.setKeepAlive(true);
+
+        //对于close关闭操作行为进行怎样的处理,默认为false 0
+        //false 0:默认情况,关闭时立即返回,底层系统接管输出流,将缓冲区内的数据发送完成
+        //true  0:关闭时立即返回,缓冲区数据抛弃,直接发送RST结束命令到对方,并无需经过2MSL等待
+        //true 20:关闭时最长阻塞20ms,这期间数据可以发送,发送不完按第二种情况处理
+        socket.setSoLinger(true, 20);
+
+        //是否让紧急数据内敛,默认false,紧急数据通过socket.setUrgentData(1)发送
+        socket.setOOBInline(false);
+
+        //设置接收发送缓冲区大小
+        socket.setReceiveBufferSize(64 * 1024 * 1024);
+        socket.setSendBufferSize(64 * 1024 * 1024);
+
+        //设置性能参数:短连接、延迟、带宽的相对重要性
     }
 
     private static void sendData(Socket client) throws IOException{
